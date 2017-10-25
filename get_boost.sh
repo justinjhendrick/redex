d792b639940f7802549df0fa099e43d3c4233860
Justin Hendrick <jhendrick@fb.com>
2017-10-25 14:36:53 -0700

    fix path

--- get_boost.sh
+++ get_boost.sh
@@ -2,7 +2,8 @@ set -e
 set -x
 
 wget https://dl.bintray.com/boostorg/release/1.64.0/source/boost_1_64_0.tar.bz2
-tar --bzip2 -xf ~/boost_1_64_0.tar.bz2
+tar --bzip2 -xf boost_1_64_0.tar.bz2
 cd boost_1_64_0
 ./bootstrap.sh --with-libraries=all
 ./b2 install
+cd ..
