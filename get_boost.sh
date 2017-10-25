a2ac0631d2a3b8620398aba29f36b8bc065ab285
Justin Hendrick <jhendrick@fb.com>
2017-10-25 15:15:15 -0700

    fewer boost parts, libc++

--- get_boost.sh
+++ get_boost.sh
@@ -4,6 +4,6 @@ set -x
 wget https://dl.bintray.com/boostorg/release/1.64.0/source/boost_1_64_0.tar.bz2
 tar --bzip2 -xf boost_1_64_0.tar.bz2
 cd boost_1_64_0
-./bootstrap.sh --with-libraries=all
+./bootstrap.sh --with-libraries=filesystem,iostreams,program_options,regex,system
 ./b2 install
 cd ..
