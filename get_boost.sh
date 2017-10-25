91b2d703d8f8a88a0a86c3de75eacef1dde85bd0
Justin Hendrick <jhendrick@fb.com>
2017-10-25 13:31:49 -0700

    set BOOST_ROOT try again again

--- get_boost.sh
+++ get_boost.sh
@@ -3,7 +3,9 @@ set -x
 
 cd ~
 wget https://dl.bintray.com/boostorg/release/1.64.0/source/boost_1_64_0.tar.bz2
+sudo -s # login as root
 cd /usr/include/
 tar --bzip2 -xf ~/boost_1_64_0.tar.bz2
+exit # stop being root
 export BOOST_ROOT=/usr/include/boost_1_64_0/
 cd ~
