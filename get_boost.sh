ecd081cd67079b7c88d70977ed94164386b54787
Justin Hendrick <jhendrick@fb.com>
2017-10-25 14:32:46 -0700

    actually install boost

--- get_boost.sh
+++ get_boost.sh
@@ -1,9 +1,7 @@
 set -e
 set -x
 
-cd ~
 wget https://dl.bintray.com/boostorg/release/1.64.0/source/boost_1_64_0.tar.bz2
-cd /usr/include/
 tar --bzip2 -xf ~/boost_1_64_0.tar.bz2
-echo 'BOOST_ROOT=/usr/include/boost_1_64_0/'
-cd ~
+cd boost_1_64_0
+./bootstrap.sh --with-libraries=all
