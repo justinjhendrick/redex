5ebd72e1f594880202e1bc09b41d64cb1250d38c
Justin Hendrick <jhendrick@fb.com>
2017-10-25 16:41:05 -0700

    quieter boost install

--- get_boost.sh
+++ get_boost.sh
@@ -4,5 +4,5 @@ wget https://dl.bintray.com/boostorg/release/1.64.0/source/boost_1_64_0.tar.bz2
 tar --bzip2 -xf boost_1_64_0.tar.bz2
 cd boost_1_64_0
 ./bootstrap.sh --with-libraries=filesystem,iostreams,program_options,regex,system
-./b2 install
+./b2 -d0 install
 cd ..
