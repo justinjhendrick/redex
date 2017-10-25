09d5eeecf59e8a8366a0e169ead7335cdab9ca82
Justin Hendrick <jhendrick@fb.com>
2017-10-25 13:15:31 -0700

    set BOOST_ROOT

--- get_boost.sh
+++ get_boost.sh
@@ -1,5 +1,9 @@
+set -e
+set -x
+
 cd ~
 wget https://dl.bintray.com/boostorg/release/1.64.0/source/boost_1_64_0.tar.bz2
 cd /usr/include/
 tar --bzip2 -xf ~/boost_1_64_0.tar.bz2
+export BOOST_ROOT=/usr/include/boost_1_64_0/
 cd ~
