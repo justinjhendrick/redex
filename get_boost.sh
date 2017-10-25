0ac683a409ce1395e6c5a96a0b181d6d85099b1a
Justin Hendrick <jhendrick@fb.com>
2017-10-25 14:33:08 -0700

    actually actually install boost

--- get_boost.sh
+++ get_boost.sh
@@ -5,3 +5,4 @@ wget https://dl.bintray.com/boostorg/release/1.64.0/source/boost_1_64_0.tar.bz2
 tar --bzip2 -xf ~/boost_1_64_0.tar.bz2
 cd boost_1_64_0
 ./bootstrap.sh --with-libraries=all
+./b2 install
